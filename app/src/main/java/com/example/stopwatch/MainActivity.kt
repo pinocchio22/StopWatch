dc2d59a7bd1d77c8566622ef5499b01921c158a2
pinocchio22 <vviian.2@gmail.com>
2022-06-07 17:04:01 +0900

    버튼에 이벤트 연결

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,12 +2,40 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.stopwatch.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    var isRunning = false
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        binding.btnStart.setOnClickListener(this)
+        binding.btnRefresh.setOnClickListener(this)
+    }
 
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_start -> if (isRunning) pause() else start()
+            R.id.btn_refresh -> refresh()
+        }
+    }
+
+    private fun start() {
+        // 측정시작
+    }
+
+    private fun pause() {
+        // 측정 정지
+    }
+
+    private fun refresh() {
+        // 초기화
     }
 }
